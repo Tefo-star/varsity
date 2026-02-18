c235df1354e49a93c38de434e2794626128c122e
Tefo <tkeletile@gmail.com>
2026-02-18 11:05:48 +0200

    Fix admin.py to use academic_year and year_level instead of year

--- resources/admin.py
+++ resources/admin.py
@@ -5,14 +5,14 @@ from .models import University, Course, Module, ResourceType, Resource, Resource
 class UniversityAdmin(admin.ModelAdmin):
     list_display = ['name', 'code', 'course_count']
     search_fields = ['name', 'code']
-    prepopulated_fields = {'code': ('name',)}  # Auto-generate code from name
+    prepopulated_fields = {'code': ('name',)}
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'university']
     list_filter = ['university']
     search_fields = ['code', 'name']
-    prepopulated_fields = {'code': ('name',)}  # Auto-generate code from name
+    prepopulated_fields = {'code': ('name',)}
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
@@ -27,13 +27,27 @@ class ResourceTypeAdmin(admin.ModelAdmin):
 
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ['title', 'course', 'resource_type', 'year', 'downloads']
-    list_filter = ['course__university', 'course', 'resource_type', 'year']
+    list_display = ['title', 'course', 'resource_type', 'year_level', 'academic_year', 'downloads']
+    list_filter = ['course__university', 'course', 'resource_type', 'year_level', 'academic_year']
     search_fields = ['title', 'description']
     readonly_fields = ['file_size', 'downloads', 'uploaded_at']
+    list_editable = ['year_level', 'academic_year']
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'description', 'resource_type', 'file')
+        }),
+        ('Organization', {
+            'fields': ('university', 'course', 'module', 'year_level', 'academic_year', 'semester')
+        }),
+        ('Metadata', {
+            'fields': ('file_size', 'downloads', 'uploaded_by', 'uploaded_at'),
+            'classes': ('collapse',)
+        }),
+    )
     
     def save_model(self, request, obj, form, change):
-        if not obj.pk:  # Only on creation
+        if not obj.pk:
             obj.uploaded_by = request.user
         super().save_model(request, obj, form, change)
 
