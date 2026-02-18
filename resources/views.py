82980909d53b9d88021fc3f7e067e73d129ca180
Tefo <tkeletile@gmail.com>
2026-02-18 08:43:55 +0200

    Add all missing view functions

--- resources/views.py
+++ resources/views.py
@@ -1,12 +1,100 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.core.management import call_command
 from django.conf import settings
+from .models import University, Course, Module, Resource, ResourceType
+
+def resources_dashboard(request):
+    """Main resources page - list all universities"""
+    universities = University.objects.all()
+    context = {
+        'universities': universities,
+    }
+    return render(request, 'resources/dashboard.html', context)
+
+def university_detail(request, uni_code):
+    """Show courses for a specific university"""
+    university = get_object_or_404(University, code=uni_code)
+    courses = university.courses.all()
+    context = {
+        'university': university,
+        'courses': courses,
+    }
+    return render(request, 'resources/university_detail.html', context)
+
+def course_detail(request, uni_code, course_code):
+    """Show modules and years for a course"""
+    university = get_object_or_404(University, code=uni_code)
+    course = get_object_or_404(Course, university=university, code=course_code)
+    
+    modules = course.modules.filter(parent_module=None)
+    years = Resource.objects.filter(course=course).values_list('year', flat=True).distinct().order_by('-year')
+    
+    context = {
+        'university': university,
+        'course': course,
+        'modules': modules,
+        'years': years,
+    }
+    return render(request, 'resources/course_detail.html', context)
+
+def module_detail(request, uni_code, course_code, module_id):
+    """Show submodules and resources in a module"""
+    university = get_object_or_404(University, code=uni_code)
+    course = get_object_or_404(Course, university=university, code=course_code)
+    module = get_object_or_404(Module, id=module_id, course=course)
+    
+    submodules = module.submodules.all()
+    resources = Resource.objects.filter(module=module)
+    resource_types = ResourceType.objects.all()
+    
+    resources_by_type = {}
+    for rtype in resource_types:
+        resources_by_type[rtype.name] = resources.filter(resource_type=rtype)
+    
+    context = {
+        'university': university,
+        'course': course,
+        'module': module,
+        'submodules': submodules,
+        'resources_by_type': resources_by_type,
+    }
+    return render(request, 'resources/module_detail.html', context)
+
+def year_detail(request, uni_code, course_code, year):
+    """Show all resources for a specific year"""
+    university = get_object_or_404(University, code=uni_code)
+    course = get_object_or_404(Course, university=university, code=course_code)
+    
+    resources = Resource.objects.filter(course=course, year=year)
+    modules = Module.objects.filter(course=course)
+    
+    context = {
+        'university': university,
+        'course': course,
+        'year': year,
+        'resources': resources,
+        'modules': modules,
+    }
+    return render(request, 'resources/year_detail.html', context)
+
+def view_pdf(request, resource_id):
+    """View PDF in browser"""
+    resource = get_object_or_404(Resource, id=resource_id)
+    return redirect(resource.file.url)
+
+def download_resource(request, resource_id):
+    """Download the file"""
+    resource = get_object_or_404(Resource, id=resource_id)
+    resource.downloads += 1
+    resource.save()
+    return redirect(resource.file.url + '?fl_attachment=true')
 
 def run_migrations(request):
+    """Temporary view to run migrations"""
     if not settings.DEBUG:
         return HttpResponse("Not allowed", status=403)
     
-    # Run migrations
     call_command('migrate', 'resources')
     
     return HttpResponse("""
